7e41b143f60f4feb041f3e3f1baa6f5737c5be36
Varun Kaushal <varunkaushal@uchicago.edu>
2014-03-04 19:24:02 -0600

    Initial commit of Project Framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MessageSystem::Application.config.secret_key_base = 'abf15acbf6e2be59062b31c0b10177f7f2e916a9aa87a7cf690ae952204701d16c5611517da359a3571ed1409a677ba9ef4f947dd4691bf93ea323f615f86cf1'
